4dd3eba55a990fb3d54aef060786ef1b32471d05
gabrielbosio <gabrielbosio95@gmail.com>
2026-02-04 18:46:06 -0300

    Remove dead draw methods from UI components

--- src/ui/dialog.rs
+++ src/ui/dialog.rs
@@ -2,7 +2,7 @@ use macroquad::prelude::*;
 
 use crate::config::*;
 use crate::input::{get_dialog_action, EditorAction};
-use crate::render::{BitmapFont, DrawHelpers};
+use crate::render::DrawHelpers;
 
 /// Dialog for prompting user input (filename)
 pub struct InputDialog {
@@ -64,94 +64,6 @@ impl InputDialog {
         }
 
         None
-    }
-
-    #[allow(dead_code)]
-    pub fn draw(&self, font: &BitmapFont) {
-        if !self.visible {
-            return;
-        }
-
-        // Draw dialog background
-        let dialog_width = 28 * TILE_WIDTH;
-        let dialog_height = 5 * TILE_HEIGHT;
-        let dialog_x = ((SCREEN_WIDTH - dialog_width) / 2) as f32;
-        let dialog_y = ((SCREEN_HEIGHT - dialog_height) / 2) as f32;
-
-        // Background
-        draw_rectangle(
-            dialog_x,
-            dialog_y,
-            dialog_width as f32,
-            dialog_height as f32,
-            COLOR_BLACK,
-        );
-
-        // Border
-        draw_rectangle_lines(
-            dialog_x,
-            dialog_y,
-            dialog_width as f32,
-            dialog_height as f32,
-            2.0,
-            COLOR_WHITE,
-        );
-
-        // Title
-        let title_x = dialog_x + TILE_WIDTH as f32;
-        let title_y = dialog_y + TILE_HEIGHT as f32;
-        for (i, c) in self.title.chars().enumerate() {
-            font.draw_char_with_shadow(
-                c,
-                title_x + (i as f32 * TILE_WIDTH as f32),
-                title_y,
-                COLOR_WHITE,
-                COLOR_GRAY,
-            );
-        }
-
-        // Input field background
-        let input_x = dialog_x + TILE_WIDTH as f32;
-        let input_y = dialog_y + (3 * TILE_HEIGHT) as f32;
-        let input_width = (dialog_width - 2 * TILE_WIDTH) as f32;
-
-        draw_rectangle(
-            input_x,
-            input_y,
-            input_width,
-            TILE_HEIGHT as f32,
-            COLOR_GRAY,
-        );
-
-        // Input text (truncate if too long)
-        let max_chars = ((input_width / TILE_WIDTH as f32) as usize).saturating_sub(1);
-        let display_input: String = if self.input.len() > max_chars {
-            self.input
-                .chars()
-                .skip(self.input.len() - max_chars)
-                .collect()
-        } else {
-            self.input.clone()
-        };
-
-        for (i, c) in display_input.chars().enumerate() {
-            font.draw_char(
-                c,
-                input_x + (i as f32 * TILE_WIDTH as f32),
-                input_y,
-                COLOR_WHITE,
-            );
-        }
-
-        // Cursor
-        let cursor_x = input_x + (display_input.len() as f32 * TILE_WIDTH as f32);
-        draw_rectangle(
-            cursor_x,
-            input_y,
-            TILE_WIDTH as f32,
-            TILE_HEIGHT as f32,
-            COLOR_WHITE,
-        );
     }
 
     pub fn draw_scaled(&self, helpers: &DrawHelpers) {
@@ -278,65 +190,6 @@ impl ConfirmDialog {
         None
     }
 
-    #[allow(dead_code)]
-    pub fn draw(&self, font: &BitmapFont) {
-        if !self.visible {
-            return;
-        }
-
-        // Draw dialog background
-        let dialog_width = 28 * TILE_WIDTH;
-        let dialog_height = 4 * TILE_HEIGHT;
-        let dialog_x = ((SCREEN_WIDTH - dialog_width) / 2) as f32;
-        let dialog_y = ((SCREEN_HEIGHT - dialog_height) / 2) as f32;
-
-        // Background
-        draw_rectangle(
-            dialog_x,
-            dialog_y,
-            dialog_width as f32,
-            dialog_height as f32,
-            COLOR_BLACK,
-        );
-
-        // Border
-        draw_rectangle_lines(
-            dialog_x,
-            dialog_y,
-            dialog_width as f32,
-            dialog_height as f32,
-            2.0,
-            COLOR_WHITE,
-        );
-
-        // Message
-        let msg_x = dialog_x + TILE_WIDTH as f32;
-        let msg_y = dialog_y + TILE_HEIGHT as f32;
-        for (i, c) in self.message.chars().enumerate() {
-            font.draw_char_with_shadow(
-                c,
-                msg_x + (i as f32 * TILE_WIDTH as f32),
-                msg_y,
-                COLOR_WHITE,
-                COLOR_GRAY,
-            );
-        }
-
-        // Instructions
-        let hint = "(y/n)";
-        let hint_x = dialog_x + TILE_WIDTH as f32;
-        let hint_y = dialog_y + (2.5 * TILE_HEIGHT as f32);
-        for (i, c) in hint.chars().enumerate() {
-            font.draw_char_with_shadow(
-                c,
-                hint_x + (i as f32 * TILE_WIDTH as f32),
-                hint_y,
-                COLOR_GRAY,
-                COLOR_BLACK,
-            );
-        }
-    }
-
     pub fn draw_scaled(&self, helpers: &DrawHelpers) {
         if !self.visible {
             return;
@@ -425,65 +278,6 @@ impl MessageDialog {
         }
 
         false
-    }
-
-    #[allow(dead_code)]
-    pub fn draw(&self, font: &BitmapFont) {
-        if !self.visible {
-            return;
-        }
-
-        // Draw dialog background
-        let dialog_width = 28 * TILE_WIDTH;
-        let dialog_height = 4 * TILE_HEIGHT;
-        let dialog_x = ((SCREEN_WIDTH - dialog_width) / 2) as f32;
-        let dialog_y = ((SCREEN_HEIGHT - dialog_height) / 2) as f32;
-
-        // Background
-        draw_rectangle(
-            dialog_x,
-            dialog_y,
-            dialog_width as f32,
-            dialog_height as f32,
-            COLOR_BLACK,
-        );
-
-        // Border
-        draw_rectangle_lines(
-            dialog_x,
-            dialog_y,
-            dialog_width as f32,
-            dialog_height as f32,
-            2.0,
-            COLOR_WHITE,
-        );
-
-        // Message
-        let msg_x = dialog_x + TILE_WIDTH as f32;
-        let msg_y = dialog_y + TILE_HEIGHT as f32;
-        for (i, c) in self.message.chars().enumerate() {
-            font.draw_char_with_shadow(
-                c,
-                msg_x + (i as f32 * TILE_WIDTH as f32),
-                msg_y,
-                COLOR_WHITE,
-                COLOR_GRAY,
-            );
-        }
-
-        // Instructions
-        let hint = "(press any key)";
-        let hint_x = dialog_x + TILE_WIDTH as f32;
-        let hint_y = dialog_y + (2.5 * TILE_HEIGHT as f32);
-        for (i, c) in hint.chars().enumerate() {
-            font.draw_char_with_shadow(
-                c,
-                hint_x + (i as f32 * TILE_WIDTH as f32),
-                hint_y,
-                COLOR_GRAY,
-                COLOR_BLACK,
-            );
-        }
     }
 
     pub fn draw_scaled(&self, helpers: &DrawHelpers) {
